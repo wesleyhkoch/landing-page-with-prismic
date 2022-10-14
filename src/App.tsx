c11fbb1e95d440be558697b0799b791ceb0df711
Wesley Koch <wesleykoch2004@gmail.com>
2022-10-14 17:11:06 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus at non, porro reprehenderit distinctio pariatur rerum illum dolorum, recusandae assumenda omnis repellat in sunt esse quo. Vel velit ut iusto porro exercitationem ullam deserunt id illo quos ipsa, neque perferendis ipsam veritatis. Assumenda, numquam blanditiis eos reprehenderit quidem provident pariatur adipisci asperiores fugit quisquam aut atque, placeat laudantium sit aliquam consequuntur dicta excepturi quod molestiae dolor aspernatur nesciunt hic! Corporis omnis nam blanditiis.</h1>
+    </div>
+  )
+}
