47d4db13378567260208b0e3f3b2b442ba002c4e
Wesley Koch <wesleykoch2004@gmail.com>
2022-10-17 18:53:50 -0300

    refact: organizing app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-export const App = () => {
+import { Header } from './components'
+import { Home } from './pages'
+
+function App() {
   return (
     <div>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus at non, porro reprehenderit distinctio pariatur rerum illum dolorum, recusandae assumenda omnis repellat in sunt esse quo. Vel velit ut iusto porro exercitationem ullam deserunt id illo quos ipsa, neque perferendis ipsam veritatis. Assumenda, numquam blanditiis eos reprehenderit quidem provident pariatur adipisci asperiores fugit quisquam aut atque, placeat laudantium sit aliquam consequuntur dicta excepturi quod molestiae dolor aspernatur nesciunt hic! Corporis omnis nam blanditiis.</h1>
+      <Header />
+      <Home />
     </div>
   )
 }
+
+export default App
