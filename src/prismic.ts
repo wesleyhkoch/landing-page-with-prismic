e03349125b196cb2e3feda7b6cd92395602ba14f
Wesley Koch <wesleykoch2004@gmail.com>
2022-10-17 18:52:03 -0300

    feat: creating prismic file

---
+++ src/prismic.ts
@@ -0,0 +1,8 @@
+import * as prismic from '@prismicio/client'
+ 
+export const repositoryName = '3035tech-prismictest'
+ 
+export const client = prismic.createClient(repositoryName, {
+  accessToken: 'MC5ZMDJmTGhRQUFDVUFPVkth.77-9GO-_ve-_vQ7vv73vv73vv71O77-977-9Su-_ve-_ve-_vTHvv70Z77-9e--_ve-_vWswVe-_vUBoJO-_vWPvv70'
+})
+ 
